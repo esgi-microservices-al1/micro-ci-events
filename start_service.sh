0e844b15982a4d1c9ca6379e606cfbfa274ecfa1
Joe Chatelin <josephchatelin@gmail.com>
2020-07-15 09:28:10 +0200

    update start_service.sh

--- start_service.sh
+++ start_service.sh
@@ -2,4 +2,4 @@
 docker-compose -f .github/azure/docker-compose.yml stop
 docker-compose -f .github/azure/docker-compose.yml rm -f
 docker-compose -f .github/azure/docker-compose.yml build &&
-docker-compose -f .github/azure/docker-compose.yml up
+docker-compose -f .github/azure/docker-compose.yml up -d
